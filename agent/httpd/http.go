3592cd5d26d44e39568e0a84609721fa44c45ead
kun <oiooj@qq.com>
2016-12-23 12:29:14 +0800

    format message

--- agent/httpd/http.go
+++ agent/httpd/http.go
@@ -252,6 +252,6 @@ func Start(listen string) {
 	http.HandleFunc("/me/ns", GetNsHandler)
 	http.HandleFunc("/me/status", GetStatusHandler)
 	//http.HandleFunc("/log/offset", LogOffsetHandler)
-	fmt.Println("starting collect module http listener... on ", common.Conf.Listen)
+	//fmt.Println("starting collect module http listener... on ", common.Conf.Listen)
 	log.Fatal(http.ListenAndServe(listen, nil))
 }
