b202b0074991fb1b5c716f3f427cc8335c510ff0
kun <oiooj@qq.com>
2017-04-06 18:35:22 +0800

    update proc cpu

--- agent/sysinfo/procs.go
+++ agent/sysinfo/procs.go
@@ -17,6 +17,8 @@ type ProcCollector struct{}
 var (
 	rBytes       map[int]uint64
 	wBytes       map[int]uint64
+	CPUTotal     map[int]float64
+	CPUProc      map[int]float64
 	lastProcTime time.Time
 )
 
@@ -36,30 +38,42 @@ func (self ProcCollector) Run() {
 
 	newRBytes := make(map[int]uint64)
 	newWBytes := make(map[int]uint64)
+	newCPUTotal := make(map[int]float64)
+	newCPUProc := make(map[int]float64)
 	interval := float64(time.Now().Unix() - lastProcTime.Unix())
 	lastProcTime = time.Now()
 	for _, p := range ps {
 		newRBytes[p.Pid] = p.RBytes
 		newWBytes[p.Pid] = p.WBytes
+		newCPUTotal[p.Pid] = p.TotalCpu
+		newCPUProc[p.Pid] = p.Cpu
 	}
 	for _, proc := range procs {
 		var cnt int
 		var fdNum int
 		var memory uint64
-		var cpu float64
-		var tcpEstablished int
+		var cpu, procCpuTime, totalCpuTime float64
 		var ioWrite, ioRead uint64
 		for _, p := range ps {
 			if proc.BinaryPath == p.Exe {
 				cnt++
 				memory += p.Mem
-				cpu += p.Cpu
+
+				if totalCpuOld, _ := CPUTotal[p.Pid]; totalCpuOld <= p.TotalCpu {
+					totalCpuTime = p.TotalCpu - totalCpuOld
+				}
+				if cpuOld, _ := CPUProc[p.Pid]; cpuOld <= p.Cpu {
+					procCpuTime = p.Cpu - cpuOld
+				}
+				if totalCpuTime > 0 {
+					cpu += procCpuTime / totalCpuTime
+				}
+
 				fdNum += p.FdCount
-				tcpEstablished += p.TcpEstab
-				if rOld := rBytes[p.Pid]; rOld <= p.RBytes {
+				if rOld, _ := rBytes[p.Pid]; rOld <= p.RBytes {
 					ioRead += p.RBytes - rOld
 				}
-				if wOld := wBytes[p.Pid]; wOld <= p.WBytes {
+				if wOld, _ := wBytes[p.Pid]; wOld <= p.WBytes {
 					ioWrite += p.WBytes - wOld
 				}
 			}
@@ -80,6 +94,8 @@ func (self ProcCollector) Run() {
 	}
 	rBytes = newRBytes
 	wBytes = newWBytes
+	CPUTotal = newCPUTotal
+	CPUProc = newCPUProc
 	for k, v := range m {
 		outputs.SendMetrics(common.TYPE_PROC, k, v)
 	}
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #/bin/bash 
 
-version="0.1.1"
+version="0.1.2"
 commit=`git rev-parse HEAD`
 branch=`git rev-parse --abbrev-ref HEAD`
 t=`date "+%Y-%m-%d_%H:%M:%S"`
