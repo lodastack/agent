4c3b411c25562c8e9919ee76c62b79e58aade7a4
kun <oiooj@qq.com>
2018-08-28 16:29:57 +0800

    update to go1.11

--- circle-test.sh
+++ circle-test.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-go version
-
-# install dep
-
-go get github.com/golang/dep && cd ~/.go_workspace/src/github.com/golang/dep/cmd/dep && go build -v && sudo cp dep /usr/local/bin/
-cd ${HOME}/.go_workspace/src/github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME && dep ensure -v
-
+export GO111MODULE=on
+go mod download
+go mod verify
 go test -timeout 60s -v ./...
-
 # build
-cd ./cmd/agent/
-go build -v
+cd ./cmd/agent/ && go build -v -mod readonly
